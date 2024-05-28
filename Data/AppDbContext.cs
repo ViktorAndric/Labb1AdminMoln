2711e59c4930da6226eb47956b7b4eeef65835ff
ViktorAndric <viktor@andric.nu>
2024-05-28 13:43:40 +0200

    Add project files.

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using System.Reflection.Emit;
+using System.Xml;
+using Labb1AdminMoln.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Labb1AdminMoln.Data;
+
+public class AppDbContext : DbContext
+{
+    public AppDbContext(DbContextOptions options) : base(options)
+    {
+    }
+
+    public DbSet<User> Users { get; set; }
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        base.OnModelCreating(modelBuilder);
+
+    }
+
+}
---
+++ Models/User.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Labb1AdminMoln.Models;
+
+public class User
+{
+	public Guid Id { get; set; }
+	[Required]
+	public string Name { get; set; }
+	[Required]
+	[Range(1, 120)]
+	public int Age { get; set; }
+}
